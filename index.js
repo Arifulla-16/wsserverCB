5c3bedaf6bb0a0968f1f0cc260599ed4ad8fe432
Arifulla-16 <skarfistark@gmail.com>
2022-12-15 04:02:02 +0530

    websoc fix

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const bodyParser =require("body-parser");
+const WebSocket = require("ws");
 const Mongoose = require("mongoose");
 
 Mongoose.set('strictQuery', true);
 const { urlencoded } = require("body-parser");
-const ejs = require("ejs");
-
 const app = express();
+
 app.use(express.static("public"));
-app.set("view engine","ejs");
 app.use(urlencoded({extended:true}));
 
 Mongoose.connect("mongodb+srv://skarfistark:arifulla616@cluster0.6yfrdov.mongodb.net/chatDB",()=>{
@@ -22,37 +21,55 @@ const chatSchema = new Mongoose.Schema({
 
 const Chat = new Mongoose.model("Chat",chatSchema);
 
-app.get("/",(req,res)=>{
-  res.render("home");
-});
+const server = require("http").createServer(app);
 
-app.post("/",(req,res)=>{
-  res.redirect(`/${req.body.uname}`);
-});
+const wss = new WebSocket.Server({ server:server });
 
-app.get("/:client",(req,res)=>{
-  Chat.find({},(err,ans)=>{
-    if(err){
-      console.log(err);
+wss.on('connection', function connection(ws,req) {
+  console.log("new connection");
+  
+  ws.on('message', function message(message) {
+    var msg = message+"";
+    if(msg.slice(0,5)===".;:--"){
+      var chat = new Chat({
+        name: ws.id,
+        text:" has left."
+      });
+      chat.save();
+      wss.clients.forEach((client)=>{
+        if(client.readyState === WebSocket.OPEN){
+          client.send(JSON.stringify({name:ws.id,msg:" has left."}));
+        }
+      });
+    }
+    else if(msg.slice(-5)==="*&#@%"){
+      ws.id=msg.slice(0,-5);
+      var chat = new Chat({
+        name: ws.id,
+        text:" has joined!!"
+      });
+      chat.save();
+      wss.clients.forEach((client)=>{
+        if(client.readyState === WebSocket.OPEN){
+          client.send(JSON.stringify({name:ws.id,msg:" has joined!!"}));
+        }
+      });
     }
     else{
-      res.render("client",{clientName:req.params.client,chats:ans});
+      var chat = new Chat({
+        name: ws.id,
+        text:message+""
+      });
+      chat.save();
+      wss.clients.forEach((client)=>{
+        if(client.readyState === WebSocket.OPEN){
+          client.send(JSON.stringify({name:ws.id,msg:message+""}));
+        }
+      });
     }
   });
-});
 
-app.get("/admin/CleArchat",(req,res)=>{
-  Chat.deleteMany({},(err)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.send({status:"deleted"});
-    }
-  });
 });
 
 
-app.listen(process.env.PORT,()=>{
-  console.log("strtd");
-});
+server.listen(process.env.PORT,()=>console.log("ws on 3000"));
